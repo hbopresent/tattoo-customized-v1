8ce989818d6ad76e183144c3b81ae6480f38ab47
Franklin Hung <hbopresent@gmail.com>
2017-08-04 17:08:50 +0800

    UPDATE

--- app/scripts/frontend/main.js
+++ app/scripts/frontend/main.js
@@ -1,26 +1,42 @@
 function Creation() {
   this.homePage = document.getElementById("homePage");
+  // font
+  this.toolbarFont = document.getElementById("toolbarFont");
+  this.fontList = document.getElementById("fontList");
+  this.toolbarFontFlag = false;
   // skin color
-  this.skincolor = document.getElementById("skincolor");
+  this.toolbarSkincolor = document.getElementById("toolbarSkincolor");
   this.skincolorPicker = document.getElementById("skincolorPicker");
   // text color
   this.colorSelector = document.getElementById("colorSelector");
+  this.colorInitState = document.getElementById("colorInitState");
+  this.colorSelectorFlag = false;
   // text length
   this.letterAmount = document.getElementById("letterAmount");
   // creation panel
   this.creationPanelSkin = document.getElementById("creationPanelSkin");
   this.creationPanelTattoo = document.getElementById("creationPanelTattoo");
   // show the work
-  this.displayWork = document.getElementById("displayWork");
-  this.displayWorkContent = document.getElementById("displayWorkContent");
-  this.displayWorkClose = document.getElementById("displayWorkClose");
-  this.displayButton = document.getElementById("displayButton");
+  // this.displayWork = document.getElementById("displayWork");
+  // this.displayWorkContent = document.getElementById("displayWorkContent");
+  // this.displayWorkClose = document.getElementById("displayWorkClose");
+  // this.displayButton = document.getElementById("displayButton");
 }
 
 Creation.prototype = {
   init: function() {
+    if(this.toolbarFontFlag == false) {
+      this.toolbarFont.style.fontSize = "1rem";
+    }
+    // open font list
+    this.toolbarFont.addEventListener("click", this.openFontList.bind(Creation));
+    // set font handler
+    for(var i = 0 ; i < this.fontList.children.length ; i++) {
+      document.getElementById("font" + i).addEventListener("click", this.selectFont.bind(Creation));
+    }
+
     // open skin color picker
-    this.skincolor.addEventListener("click", this.openSkincolorPicker.bind(Creation));
+    this.toolbarSkincolor.addEventListener("click", this.openSkincolorPicker.bind(Creation));
     // set skin color handler
     for(var i = 0 ; i < this.skincolorPicker.children.length ; i++) {
       document.getElementById("color" + i).addEventListener("click", this.selectSkincolor.bind(Creation));
@@ -28,31 +44,46 @@ Creation.prototype = {
     // check letter amount
     window.addEventListener("keydown", this.countingLetters.bind(Creation));
     // set display listener
-    this.displayButton.addEventListener("click", this.showTheWork.bind(Creation));
+    // this.displayButton.addEventListener("click", this.showTheWork.bind(Creation));
     // close displayWork
-    this.displayWorkClose.addEventListener("click", this.closeTheWork.bind(Creation));
+    // this.displayWorkClose.addEventListener("click", this.closeTheWork.bind(Creation));
     // clear notification animation
     this.letterAmount.addEventListener("animationend", function(e) {
       if(e.animationName === "notification") {
         work.letterAmount.style.animation = "";
       }
     });
+    // set focus listener on creationPanelTattoo
+    this.creationPanelTattoo.addEventListener("focus", this.setTextFont.bind(Creation));
+  },
+  openFontList: function() {
+    work.fontList.style.top = "0px";
+  },
+  closeFontList: function() {
+    work.fontList.style.top = "-60px";
   },
   openSkincolorPicker: function() {
-    work.skincolorPicker.style.transform = "scale(1)";
-    work.skincolorPicker.style.display = "flex";
+    work.skincolorPicker.style.top = "0px";
   },
   closeSkincolorPicker: function() {
-    if(work.skincolorPicker.style.transform == "scale(1)") {
-      work.skincolorPicker.style.transform = "scale(0)";
-    }
+    work.skincolorPicker.style.top = "-60px";
+  },
+  selectFont: function(e) {
+    work.toolbarFontFlag = true;
+    work.toolbarFont.style.fontFamily = e.target.style.fontFamily;
+    work.toolbarFont.style.fontSize = "2.5rem";
+    work.closeFontList();
+    work.setTextFont();
+  },
+  setTextFont: function() {
+    work.creationPanelTattoo.style.fontFamily = work.toolbarFont.style.fontFamily;
   },
   selectSkincolor: function(e) {
     // set skin color on body, html, skincolor, creationPanelSkin, displayWork
-    work.skincolor.style.backgroundColor = e.target.style.backgroundColor;
+    work.toolbarSkincolor.style.backgroundColor = e.target.style.backgroundColor;
     work.homePage.style.backgroundColor = e.target.style.backgroundColor;
     work.creationPanelSkin.style.backgroundColor = e.target.style.backgroundColor;
-    work.displayWork.style.backgroundColor = e.target.style.backgroundColor;
+    // work.displayWork.style.backgroundColor = e.target.style.backgroundColor;
     document.getElementsByTagName("body")[0].style.backgroundColor = e.target.style.backgroundColor;
 
     work.closeSkincolorPicker();
@@ -64,23 +95,22 @@ Creation.prototype = {
   },
   countingLetters: function() {
     if(work.letterAmount.value > 0) {
-      work.letterAmount.value = (20 - (work.creationPanelTattoo.value.length));
+      work.letterAmount.value = (25 - (work.creationPanelTattoo.value.length));
     }
     else {
       work.letterAmount.style.animation = "notification 0.2s ease forwards";
     }
-    // work.letterAmount.value = (20 - (work.creationPanelTattoo.value.length));
   },
-  showTheWork: function() {
-    work.displayWorkContent.value = work.creationPanelTattoo.value;
-    work.displayWorkContent.style.color = work.creationPanelTattoo.style.color;
-    work.displayWork.style.visibility = "visible";
-    work.displayWork.style.zIndex = 5;
-  },
-  closeTheWork: function() {
-    work.displayWork.style.visibility = "hidden";
-    work.displayWork.style.zIndex = -1;
-  }
+  // showTheWork: function() {
+  //   work.displayWorkContent.value = work.creationPanelTattoo.value;
+  //   work.displayWorkContent.style.color = work.creationPanelTattoo.style.color;
+  //   work.displayWork.style.visibility = "visible";
+  //   work.displayWork.style.zIndex = 5;
+  // },
+  // closeTheWork: function() {
+  //   work.displayWork.style.visibility = "hidden";
+  //   work.displayWork.style.zIndex = -1;
+  // }
 };
 
 var work = new Creation();
